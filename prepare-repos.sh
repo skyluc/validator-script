637ab483034a09432e5ed6897ba09df51d01c272
François Garillot <francois@garillot.net>
2013-05-24 11:17:31 +0200

    better statefulness management for source repos
    Fixes #2

--- prepare-repos.sh
+++ prepare-repos.sh
@@ -3,11 +3,40 @@
 # We clone with the specified commit of Scala, and the latest
 # versions of sbinary, sbt, scala-refactoring and scala-ide.
 
+SCALAURL="git://github.com/scala/scala.git"
+SBINARYURL="git://github.com/scala-ide/sbinary.git"
+SBTURL="git://github.com/sbt/sbt.git"
+REFACURL="git://github.com/scala-ide/scala-refactoring.git"
+IDEURL="git://github.com/scala-ide/scala-ide.git"
+
 function usage() {
     echo "Usage : $0 [-s <scala-commit>] [-b <basedir>]"
 }
 
-CLONESCALA=""
+function getOrUpdate(){
+    local deepen=''
+    if [ ! -d $1 ]; then
+        git clone --depth 1 $2
+        deepen='true'
+    else
+        pushd $1
+        originUrl=$(git config --get remote.origin.url)
+        if [ ! $originUrl = $2 ]; then
+            echo "Can't understand repository structure in $1, aborting"
+            exit 1
+        fi
+        git fetch origin
+        popd
+    fi
+    pushd $1
+    git checkout -q $3
+    if [ $deepen ]; then
+        echo "Deepening $1 by $4 commits"
+        git fetch --depth $4
+    fi
+    popd
+}
+
 set -- $(getopt s:b: $*)
 while [ $# -gt 0 ]
 do
@@ -37,32 +66,22 @@ IDEDIR="$BASEDIR/scala-ide/"
 if [ ! -d $BASEDIR ]; then mkdir -p $BASEDIR; fi
 
 cd $BASEDIR
-if [ ! -n $SCALACOMMIT ]; then
-    # on average, 1K commits betw 2 Scala milestones
-    git clone --depth 1 git://github.com/scala/scala.git
-    pushd $SCALADIR
-    git checkout $SCALACOMMIT
-    popd
-fi
-
+# on average, 1K commits betw 2 Scala milestones
 # on average, < 10 commits betw sbinary chosen versions
-git clone --depth 1 git://github.com/scala-ide/sbinary.git
-# this depends on the fact that the default clone checkout is the
-# dev branch (master or the local equivalent)
-
 # on average, 80 commits betw 2 sbt milestones
-git clone --depth 1 git://github.com/sbt/sbt.git
-# this depends on the fact that the default clone checkout is the
-# dev branch (master or the local equivalent)
-
 # on average, 50 commits betw 2 refactoring releases
-git clone --depth 1 git://github.com/scala-ide/scala-refactoring.git
-# this depends on the fact that the default clone checkout is the
-# dev branch (master or the local equivalent)
-
 # on average, 350 commits betw 2 ide releases
-git clone --depth 1 git://github.com/scala-ide/scala-ide.git
-# this depends on the fact that the default clone checkout is the
+if [ ! -n $SCALACOMMIT ]; then
+    getOrUpdate $SCALADIR $SCALAURL $SCALACOMMIT 2000
+fi
+getOrUpdate $SBINARYDIR $SBINARYURL "origin/HEAD" 20
+# TODO : fix up some symbolic-ref detection for sbt, this is
+# gonna blow up in our face
+getOrUpdate $SBTDIR $SBTURL "origin/HEAD" 160
+getOrUpdate $REFACDIR $REFACURL "origin/HEAD" 100
+getOrUpdate $IDEDIR $IDEURL "origin/HEAD" 700
+
+# ths depends on the fact that the default clone checkout is the
 # dev branch (master or the local equivalent)
 
 cd $ORIGPWD
